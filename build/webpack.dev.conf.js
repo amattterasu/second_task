f6f27a10c592fcdf82fdb129b9b472dc0639a96c
Andrey Gubaydullin <amattterasuuu@gmail.com>
2019-10-25 21:49:50 +0600

    added new component

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -7,7 +7,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     devtool: 'cheap-module-eval-source-map',
     devServer: {
         contentBase: baseWebpackConfig.externals.paths.dist,
-        port: 1337,
+        port: 8081,
         overlay: {
             warnings: true,
             errors: true
